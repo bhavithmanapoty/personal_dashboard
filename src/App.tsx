16ab00d9414cc871a624954099d658f900c124e3
Bhavith Manapoty <catchbhav2002@gmail.com>
2026-02-01 18:48:47 -0500

    set bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import backgroundImage from './assets/background.png'
+
 function App() {
 
   return (
-    <>
+    <div 
+      style={{
+        width: '100%',
+        height: '100%',
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      }}
+    >
       <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
-    </>
+    </div>
   )
 }
 
